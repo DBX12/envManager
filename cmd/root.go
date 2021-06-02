01c18e35739f9f7f38ad1ddae6c3f238f0c61148
DBX12 <dbx12@dbx12.de>
2021-06-02 18:42:58 +0200

    Setup cobra

---
+++ cmd/root.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "envManager",
+	Short: "Manage your environment variables",
+	Long:  `A program to manage the environment variables of your shell, pulling the secrets from a secure secretsStorage like keepass.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//Run: func(cmd *cobra.Command, args []string) {
+	//	fmt.Printf("%#v",args)
+	//},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// Cobra also supports local flags, which will only runLoad
+	// when this action is called directly.
+	//rootCmd.Flags().StringArray("load",nil, "List of profiles to load")
+	rootCmd.Flags().Bool("verbose", false, "Enable verbose output")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".envManager" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".envManager")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
