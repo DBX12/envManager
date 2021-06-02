5257c0d429dba399b97911883f4bae1512b085be
DBX12 <dbx12@dbx12.de>
2021-06-02 07:58:20 +0200

    Add Environment type

---
+++ environment.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+//Environment contains the current environment, variables which will be added to it and variables which will be removed
+//from it the next time WriteStatements is called.
+type Environment struct {
+	//current holds the shell's environment after calling Load
+	current map[string]string
+	//addVars holds the variables to add with WriteStatements (will generate export key=value statements)
+	addVars map[string]string
+	//delVars holds the variable names to remove with WriteStatements (will generate unset value statements)
+	delVars map[string]bool
+}
+
+//NewEnvironment creates a new Environment object and initializes the fields with empty maps / slices
+func NewEnvironment() Environment {
+	return Environment{
+		current: map[string]string{},
+		addVars: map[string]string{},
+		delVars: map[string]bool{},
+	}
+}
+
+func (e *Environment) Load() {
+	mapped := os.Environ()
+	for _, element := range mapped {
+		parts := strings.Split(element, "=")
+		e.current[parts[0]] = strings.Join(parts[1:], "=")
+	}
+}
+
+//Set adds an environment variable with given key and value to the list of variables to set. Call WriteStatements to
+//create export statements consumable by a shell.
+func (e *Environment) Set(key string, value string) error {
+	if key == "" {
+		return errors.New("key must not be empty")
+	}
+	e.addVars[key] = value
+	return nil
+}
+
+//Unset removes an already set environment variable. Also removes it from the list of variables which will be set.
+func (e *Environment) Unset(key string) error {
+	if key == "" {
+		return errors.New("key must not be empty")
+	}
+	delete(e.addVars, key)
+	e.delVars[key] = true
+	return nil
+}
+
+//WriteStatements writes a list of export and unset statements to update the environment of a shell.
+func (e *Environment) WriteStatements() string {
+	var output []string
+	for key, value := range e.addVars {
+		output = append(output, fmt.Sprintf("export %s=\"%s\"", key, value))
+	}
+	for key, _ := range e.delVars {
+		output = append(output, fmt.Sprintf("unset %s", key))
+	}
+	return strings.Join(output, ";")
+}
---
+++ environment_test.go
@@ -0,0 +1,264 @@
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestEnvironment_Load(t *testing.T) {
+	tests := []struct {
+		name    string
+		envVars map[string]string
+	}{
+		{
+			name:    "Empty environment",
+			envVars: map[string]string{},
+		},
+		{
+			name: "One environment variable",
+			envVars: map[string]string{
+				"FOO_PATH": "/tmp/foo",
+			},
+		},
+		{
+			name: "Multiple environment variables",
+			envVars: map[string]string{
+				"FOO_PATH":  "/tmp/foo",
+				"BAR_COUNT": "5",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := prepareEnv(true, tt.envVars)
+			if err != nil {
+				t.Fatalf("Failed to prepare environment for test %s with error: %s", t.Name(), err.Error())
+			}
+			e := NewEnvironment()
+			e.Load()
+			if !reflect.DeepEqual(e.current, tt.envVars) {
+				t.Errorf("Wanted %#v but got %#v", tt.envVars, e.current)
+			}
+		})
+	}
+}
+
+func TestEnvironment_Set(t *testing.T) {
+	type args struct {
+		key   string
+		value string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "Set string pair",
+			args: args{
+				key:   "EDITOR",
+				value: "vi",
+			},
+			want: map[string]string{
+				"EDITOR": "vi",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Set empty value",
+			args: args{
+				key:   "EDITOR",
+				value: "",
+			},
+			want: map[string]string{
+				"EDITOR": "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Set empty key",
+			args: args{
+				key:   "",
+				value: "vi",
+			},
+			want:    map[string]string{},
+			wantErr: true,
+		},
+		{
+			name: "Set empty key and value",
+			args: args{
+				key:   "",
+				value: "",
+			},
+			want:    map[string]string{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := NewEnvironment()
+			err := e.Set(tt.args.key, tt.args.value)
+			if err != nil && !tt.wantErr {
+				t.Error("Got an error but wanted none")
+			} else if err == nil && tt.wantErr {
+				t.Error("Got no error but wanted one")
+			}
+			if !reflect.DeepEqual(e.addVars, tt.want) {
+				t.Errorf("Wanted %#v but got %#v", tt.want, e.addVars)
+			}
+		})
+	}
+}
+
+func TestEnvironment_Unset(t *testing.T) {
+	type fields struct {
+		current map[string]string
+		addVars map[string]string
+		delVars map[string]bool
+	}
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    fields
+		wantErr bool
+	}{
+		{
+			name: "Remove not added, not removed var",
+			fields: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{},
+			},
+			args: args{
+				key: "EDITOR",
+			},
+			want: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{"EDITOR": true},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Remove added, not removed var",
+			fields: fields{
+				current: map[string]string{},
+				addVars: map[string]string{
+					"EDITOR": "vim",
+				},
+				delVars: map[string]bool{},
+			},
+			args: args{
+				key: "EDITOR",
+			},
+			want: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{"EDITOR": true},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Remove not added, removed var",
+			fields: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{"EDITOR": true},
+			},
+			args: args{
+				key: "EDITOR",
+			},
+			want: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{"EDITOR": true},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Remove empty string",
+			fields: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{},
+			},
+			args: args{
+				key: "",
+			},
+			want: fields{
+				current: map[string]string{},
+				addVars: map[string]string{},
+				delVars: map[string]bool{},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := Environment{
+				current: tt.fields.current,
+				addVars: tt.fields.addVars,
+				delVars: tt.fields.delVars,
+			}
+			err := e.Unset(tt.args.key)
+			if err != nil && !tt.wantErr {
+				t.Error("Got an error but wanted none")
+			} else if err == nil && tt.wantErr {
+				t.Error("Got no error but wanted one")
+			}
+			if !reflect.DeepEqual(tt.want.current, e.current) {
+				t.Errorf("Unset() current = %v, want %v", e.current, tt.want.current)
+			}
+			if !reflect.DeepEqual(tt.want.addVars, e.addVars) {
+				t.Errorf("Unset() addVars = %v, want %v", e.addVars, tt.want.addVars)
+			}
+			if !reflect.DeepEqual(tt.want.delVars, e.delVars) {
+				t.Errorf("Unset() delVars = %v, want %v", e.delVars, tt.want.delVars)
+			}
+		})
+	}
+}
+
+func TestEnvironment_WriteStatements(t *testing.T) {
+	e := NewEnvironment()
+	_ = e.Set("FOO_PATH", "/tmp/foo")
+	_ = e.Set("BAR_PATH", "/tmp/bar")
+	_ = e.Unset("EDITOR")
+	want := "export FOO_PATH=\"/tmp/foo\";export BAR_PATH=\"/tmp/bar\";unset EDITOR"
+	got := e.WriteStatements()
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("WriteStatements() = %v, want %v", got, want)
+	}
+}
+
+func TestNewEnvironment(t *testing.T) {
+	e := NewEnvironment()
+	if e.addVars == nil {
+		t.Errorf("addVars was not initialized")
+	}
+	if e.delVars == nil {
+		t.Errorf("delVars was not initialized")
+	}
+	if e.current == nil {
+		t.Errorf("current was not initialized")
+	}
+}
+
+func prepareEnv(clearCurrent bool, newVars map[string]string) error {
+	if clearCurrent {
+		os.Clearenv()
+	}
+	for name, value := range newVars {
+		err := os.Setenv(name, value)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
