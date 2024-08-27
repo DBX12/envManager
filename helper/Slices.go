39c4b25134c69c1dfb0363820bbff971e1913d81
DBX12 <dbx12@dbx12.de>
2024-08-27 11:07:11 +0200

    Replace several helpers.Slice* functions with calls to the slices package from the standard library
    This also makes helpers.SliceStringLinearSearch unused.

--- helper/Slices.go
+++ helper/Slices.go
@@ -1,31 +1,18 @@
 package helper
 
+import "slices"
+
 // SliceStringContains checks if a slice of strings contains a given needle
 func SliceStringContains(needle string, haystack []string) bool {
-	return SliceStringLinearSearch(needle, haystack) != -1
+	return slices.Contains(haystack, needle)
 }
 
 // SliceStringRemove removes all occurrences of the given value from an slice of
 // strings
 func SliceStringRemove(value string, slice []string) []string {
-	var out []string
-	for i := 0; i < len(slice); i++ {
-		if slice[i] != value {
-			out = append(out, slice[i])
-		}
-	}
-	return out
-}
-
-// SliceStringLinearSearch finds the first occurrence of the given needle in the
-// haystack. Returns -1 if the needle was not found.
-func SliceStringLinearSearch(needle string, haystack []string) int {
-	for i := 0; i < len(haystack); i++ {
-		if haystack[i] == needle {
-			return i
-		}
-	}
-	return -1
+	return slices.DeleteFunc(slice, func(s string) bool {
+		return s == value
+	})
 }
 
 // SliceStringUnique removes all duplicated items in the input slice
@@ -46,25 +33,17 @@ func SliceStringUnique(input []string) []string {
 // considering the order of the items. This means [foo bar] is equal to
 // [bar foo] but not equal to [foo baz].
 func SliceStringEqual(a []string, b []string) bool {
-	lenA := len(a)
-	if lenA != len(b) {
+	// return early if the lengths do not match to avoid costly sorting and comparing
+	if len(a) != len(b) {
 		return false
 	}
-	for i := 0; i < lenA; i++ {
-		if SliceStringLinearSearch(a[i], b) == -1 {
-			// entry a[i] is missing from b, the slices are not equal
-			return false
-		}
-	}
-	return true
+	slices.Sort(a)
+	slices.Sort(b)
+	return slices.Compare(a, b) == 0
 }
 
 // SliceStringReverse reverses the input slice. If the input slice is nil, an empty slice is returned.
 func SliceStringReverse(input []string) []string {
-	length := len(input)
-	output := make([]string, length)
-	for i := 0; i < length; i++ {
-		output[length-i-1] = input[i]
-	}
-	return output
+	slices.Reverse(input)
+	return input
 }
